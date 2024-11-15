c190e4022f5811d01dd13c545f15f8d6e5a1d8da
jzanchi2026 <jzanchi2026@jpkeefehs.org>
2024-11-14 21:52:05 -0500

    Restarted lol

--- JavaScript/stagefright.js
+++ JavaScript/stagefright.js
@@ -1,1 +1,21 @@
-// hello world
+// cool but find a way to make it so it only happens closer to the middle of pic
+document.addEventListener('DOMContentLoaded', () => {
+    let gg = document.getElementById("bgg");
+    let cursorRing = document.querySelector('.cursor-ring');
+	
+    if (gg) {
+        gg.addEventListener('mouseenter', () => {
+            cursorRing.style.opacity = 0.2;
+        });
+
+        gg.addEventListener('mouseleave', () => {
+            cursorRing.style.opacity = 0;
+        });
+
+        gg.addEventListener('mousemove', (event) => {
+            const { clientX, clientY } = event;
+            cursorRing.style.left = `${clientX - 50}px`;
+            cursorRing.style.top = `${clientY - 50}px`;
+        });
+    }
+});
